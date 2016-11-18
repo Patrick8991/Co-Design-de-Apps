d3cfb887236daff51a6d23d009b1484ab47145fe
Gabriel Moreira <gabriel.moreira98@hotmail.com>
2016-11-18 00:33:43 -0200

    App normal

---
+++ APP/test.js
@@ -0,0 +1,14 @@
+function checkUser(){
+    var elMsg = document.getElementById('feedback');
+    var elUsername = document.getElementById('nome');
+    if (elUsername.value.length<4){
+        elMsg.textContent = 'O nome deve ser maior!';
+        }
+    else{
+        elMsg.textContent ='';
+    }
+}
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
